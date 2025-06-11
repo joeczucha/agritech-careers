32bde2b2d218c052de0088cc38a3b09475b7117f
Joe Czucha <hello@doctypedigital.ie>
2025-06-11 21:47:50 +0100

    application model, migration and factory

---
+++ database/migrations/2025_06_11_195020_create_applications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('linkedin_profile');
+            $table->string('phone');
+            $table->string('location');
+            $table->text('cover_note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};
