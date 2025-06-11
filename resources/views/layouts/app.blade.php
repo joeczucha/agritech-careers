b09c1060d15ca2ba3a109df6ef43f0ad87973536
Joe Czucha <hello@doctypedigital.ie>
2025-06-11 20:46:58 +0100

    downgrading tailwind and installing filament

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,21 @@
-<html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <title>App Name - @yield('title')</title>
+    <meta charset="utf-8">
+
+    <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }}</title>
+
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
+
+    @filamentStyles
     @vite('resources/css/app.css')
 </head>
 
@@ -11,6 +25,9 @@
     @yield('content')
 
     @include('partials.footer')
+
+    @filamentScripts
+    @vite('resources/js/app.js')
 </body>
 
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+import preset from "./vendor/filament/support/tailwind.config.preset";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    presets: [preset],
+    content: [
+        "./app/Filament/**/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./vendor/filament/**/*.blade.php",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Arial", "Helvetica Neue", "Helvetica", "sans-serif"],
+            },
+            colors: {
+                slate: "#666666",
+                blue: "#2f4d91",
+                green: "#3d9c46",
+            },
+            container: {
+                center: true,
+                padding: "1rem",
+                screens: {
+                    lg: "1000px",
+                },
+            },
+        },
+    },
+    plugins: [],
+};
