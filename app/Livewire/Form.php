372cbdf0add2207567c2e9a73779c158ffe610ea
Joe Czucha <hello@doctypedigital.ie>
2025-06-13 14:58:33 +0100

    sending mail

--- app/Livewire/Form.php
+++ app/Livewire/Form.php
@@ -3,6 +3,8 @@
 namespace App\Livewire;
 
 // use Filament\Forms\Components\Button;
+use App\Mail\ApplicationMailer;
+use App\Models\Application;
 use Filament\Actions\Action;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\FileUpload;
@@ -12,6 +14,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Component;
 
 class Form extends Component implements HasForms
@@ -90,9 +93,11 @@ class Form extends Component implements HasForms
 
     public function create(): void
     {
-        Action::make('confirm')
-            ->action(fn($record) => dd($this->form->getState()))
-            ->requiresConfirmation();
+        $data = $this->form->getState();
+
+        $record = Application::create($data);
+
+        Mail::to('hello@doctypedigital.ie')->send(new ApplicationMailer($record));
     }
 
     public function render()
