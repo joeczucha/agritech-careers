3a167fb407ac6338faf9926ee23ece31d1cb07a8
Joe Czucha <hello@doctypedigital.ie>
2025-06-17 23:45:01 +0100

    header and footer styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,87 @@
-<footer>
-    <div class="min-h-[540px] bg-[#21397D]"></div>
+<footer class="border-top: 2px solid rgba(255, 255, 255, 0.5) text-white text-sm font-bold">
+    <div
+        class="py-[64px] bg-[#21397D] bg-[url('https://agritech.ie/wp-content/uploads/2016/07/logo-bg.png')] bg-no-repeat bg-center bg-contain">
+        <div class="container mx-auto grid grid-cols-12 gap-[60px]">
+            <div class="col-span-3">
+                <h3 class="text-2xl mb-4">Get In Touch</h3>
+                <p>AGRITECH</p>
+                <p>
+                    Ballyanny, Nenagh,<br />
+                    Co. Tipperary.
+                </p>
+                <p class="mt-6">Postcode: E45 RK11</p>
+                <p class="mt-6"><a href="tel:+3536731590">T: +353 (0) 67 31590</a></p>
+                <p><a href="tel:+3536733355">F: +353 (0) 67 33355</a></p>
+                <p><a href="mailto:info@agritech.ie">E: info@agritech.ie</a></p>
+
+            </div>
+
+            <div class="col-span-3 ">
+                <h3 class="text-2xl mb-4">Links</h3>
+                <ul class="">
+                    <li class="mb-2"><a href="https://agritech.ie/nutrition-and-forage-products/">Our Products</a>
+                    </li>
+                    <li class="mb-2"><a href="https://agritech.ie/?page_id=104">Technical</a></li>
+                    <li class="mb-2"><a href="https://agritech.ie/international/">International</a></li>
+                    <li class="mb-2"><a href="https://agritech.ie/updates/">News</a></li>
+                    <li class="mb-2"><a href="https://agritech.ie/find-a-distributor/">Find Your Local Sales
+                            Representative/Distributor</a></li>
+                    <li class="mb-2"><a href="http://customer.agritech.ie/Account/Login.aspx?ReturnUrl=%2f">Customer
+                            Login</a></li>
+                    <li class="mb-2"><a href="https://agritech.ie/company-info/">About</a></li>
+                    <li class="mb-2"><a href="https://agritech.ie/recruitment/">Recruitment</a></li>
+                    <li class="mb-2"><a href="https://agritech.ie/head-office-2/">Contact Us</a></li>
+                </ul>
+            </div>
+
+            <div class="col-span-4">
+                <h3 class="text-2xl mb-4">Latest News</h3>
+                <ul>
+                    <li class="mb-4">
+                        <a href="https://agritech.ie/achieving-replacement-heifer-weights/" class="flex flex-col">
+                            <span class="text-[#a7c2d7] text-base">Achieving replacement heifer
+                                weights</span>
+                            <span class="text-xs ml-4">read more »</span>
+                        </a>
+                    </li>
+                    <li class="mb-4">
+                        <a href="https://agritech.ie/why-quality-silage-matters/" class="flex flex-col"><span
+                                class="text-[#a7c2d7] text-base">Why
+                                quality
+                                silage
+                                matters</span>
+                            <span class="text-xs ml-4">read more »</span>
+                        </a>
+                    </li>
+                    <li class="mb-4">
+                        <a href="https://agritech.ie/the-benefits-of-reseeding/" class="flex flex-col"><span
+                                class="text-[#a7c2d7] text-base">The
+                                Benefits of
+                                Reseeding</span>
+                            <span class="text-xs ml-4">read more »</span>
+                        </a>
+                    </li>
+                    <li class="mb-4">
+                        <a href="https://agritech.ie/best-practices-to-encourage-rumen-development/"
+                            class="flex flex-col"><span class="text-[#a7c2d7] text-base">Best
+                                practices to encourage rumen development</span>
+                            <span class="text-xs ml-4">read more »</span>
+                        </a>
+                    </li>
+                    <li class="mb-4">
+                        <a href="https://agritech.ie/quality-colostrum-remains-key-for-calves/"
+                            class="flex flex-col"><span class="text-[#a7c2d7] text-base">Quality Colostrum
+                                Remains Key for Calves</span>
+                            <span class="text-xs ml-4">read more »</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="container mx-auto ">
+            <p class="mt-[96px]">© {{ date('Y') }} Agritech | All rights reserved</p>
+        </div>
+
+
+    </div>
     <div class="min-h-[50px] bg-[#2D4D90]"></div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,30 @@
 <header>
-    <div class="site-info min-h-[36px] bg-green"></div>
-    <div class="site-info min-h-[90px] bg-blue"></div>
+    <div class="site-info bg-green">
+        <div class="container  flex justify-end items-center gap-4 text-white text-sm font-semibold min-h-[36px]">
+            <span>Contact Agritech
+                <a href="tel:+3536731590"><span class="et-info-skype"></span>
+                    <span id="et-info-phone">+3536731590</span>
+                </a>
+            </span>
+            <span>
+                <a href="mailto:info@agritech.ie">
+                    <span id="et-info-email">info@agritech.ie</span>
+                </a>
+            </span>
+            <span>
+                <a href="http://customer.agritech.ie/Account/Login.aspx?ReturnUrl=%2f">
+                    <span id="et-info-login">Customer Login</span>
+                </a>
+            </span>
+        </div>
+    </div>
+    <div class="site-info min-h-[90px] bg-blue py-4">
+        <div class="container mx-auto flex justify-between items-center">
+            <img src="https://agritech.ie/wp-content/uploads/2016/08/LOGO-180.jpg" alt="Agritech" class="max-w-[160px]">
+            <a href="https://www.agritech.ie/"
+                class="text-white font-bold border-2 border-white px-3 py-3 rounded-xl">Back to our
+                main
+                website</a>
+        </div>
+    </div>
 </header>
