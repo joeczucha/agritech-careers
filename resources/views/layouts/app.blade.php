0b1e998c7505b94a739a813107edb6ded00f2fe0
Joe Czucha <hello@doctypedigital.ie>
2025-06-24 17:10:19 +0100

    updating page title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name') }}</title>
     <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
     <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
 
+    <title>Careers at Agritech &amp; Forage products | {{ config('app.name') }}</title>
 
     <style>
         [x-cloak] {
