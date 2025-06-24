8bd9267e9bcb6e3fcc3280de695a80c19ca1de55
Joe Czucha <hello@doctypedigital.ie>
2025-06-24 17:10:09 +0100

    adding favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ config('app.name') }}</title>
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
+
 
     <style>
         [x-cloak] {
