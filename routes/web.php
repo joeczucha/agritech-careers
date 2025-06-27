f13b1a0e0f9c93dd6e2265419784807b251c7b93
Joe Czucha <hello@doctypedigital.ie>
2025-06-27 15:41:07 +0100

    redirect all child roots back to root

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::any('{any}', fn() => redirect('/'))->where('any', '.*');
