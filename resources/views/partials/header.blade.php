e5105af87283eaf819e43a693041475e95421953
Joe Czucha <hello@doctypedigital.ie>
2025-06-27 17:07:38 +0100

    fixing responsive header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,7 @@
 <header>
     <div class="site-info bg-green">
-        <div class="container  flex justify-end items-center gap-4 text-white text-sm font-semibold min-h-[36px]">
+        <div
+            class="container flex flex-col sm:flex-row justify-end items-center gap-2 sm:gap-4 py-2 sm:py-0 text-white text-sm font-semibold min-h-[36px]">
             <span>Contact Agritech
                 <a href="tel:+3536731590"><span class="et-info-skype"></span>
                     <span id="et-info-phone">+3536731590</span>
@@ -19,7 +20,7 @@
         </div>
     </div>
     <div class="site-info min-h-[90px] bg-blue py-4">
-        <div class="container mx-auto flex justify-between items-center">
+        <div class="container mx-auto flex flex-col gap-4 sm:flex-row justify-between items-center">
             <img src="https://agritech.ie/wp-content/uploads/2016/08/LOGO-180.jpg" alt="Agritech" class="max-w-[160px]">
             <a href="https://www.agritech.ie/"
                 class="text-white font-bold border-2 border-white px-3 py-3 rounded-xl">Back to our
